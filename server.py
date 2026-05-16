a807eb183a048a7f282f7dcc6b8637c9aa0a3f52
SatishKumar <81620964+itssatishkumar@users.noreply.github.com>
2026-05-16 18:18:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -95,4 +95,4 @@ def home():
     return "Server running"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
